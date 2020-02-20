7e0e1e787efebc729356ca570743267535b93615
Ammar Hamilcar Tijani <ammar@tijani.nl>
2020-02-20 16:01:01 +0100

    * WIP: fork

--- index.js
+++ index.js
@@ -12,17 +12,70 @@ var _pumpify = require('pumpify');
 
 var _pumpify2 = _interopRequireDefault(_pumpify);
 
+var EOL = require('os').EOL;
+
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 module.exports = function () {
 
+  var count = 0;
+  var reg = new RegExp('(WEBVTT\s*(FILE)?.*)('+EOL+')*', 'g' )
   var write = function write(line, enc, cb) {
 
     if (!line.trim()) return cb();
+    
+    if(line.match(/(\d{2}:\d{2})\.(\d{3}\s+)\-\-\>(\s+\d{2}:\d{2})\.(\d{3}\s*)/g))
+    {
+      var vttComp = line.split('-->')
+      var vttLine = vttComp.map(function(item){
+        item = item.replace('.',',');
+        if(item.split(":").length < 3)
+        {
+          item = '00:'+item.trim(); 
+        }  
+        return item;
+      }).join(' --> ');
+      vttLine = vttLine+EOL
+    }
+    else if(line.match(/(\d{2}:\d{2}:\d{2})\.(\d{3}\s+)\-\-\>(\s+\d{2}:\d{2}:\d{2})\.(\d{3}\s*)/g))
+    {
+      var vttComp = line.split('-->')
+      var vttLine = vttComp.map(function(item){
+        item = item.replace('.',',');
+        if(item.split(":").length < 3)
+        {
+          item = '00:'+item.trim(); 
+        }  
+        return item;
+      }).join(' --> ');
+      vttLine = EOL+vttLine+EOL
+    }
+    else if(line.match(reg))
+    {
+      var vttLine = line.replace(reg, '')
+    }
+    else
+    {
+      var vttLine = line+EOL;
+    }     
+                  
 
-    var vttLine = line.replace(/(WEBVTT\s*(FILE)?.*)(\r\n)*/g, '').replace(/(\d{2}:\d{2}:\d{2})\.(\d{3}\s+)\-\-\>(\s+\d{2}:\d{2}:\d{2})\.(\d{3}\s*)/g, '$1,$2-->$3,$4').replace(/\<.+\>(.+)/g, '$1').replace(/\<.+\>(.+)\<.+\/\>/g, '$1') + '\r\n';
+                  
+
 
     if (!vttLine.trim()) return cb();
+
+        if (/^Kind:|^Language:/m.test(vttLine)) {
+            return cb();
+          }
+      
+         if (/^[0-9]+:/m.test(vttLine)) {
+            if (count === 0) {
+              vttLine = ++count + EOL + vttLine;
+            } else {
+              vttLine = EOL + ++count + EOL + vttLine;
+            }
+          }
 
     cb(null, vttLine);
   };
